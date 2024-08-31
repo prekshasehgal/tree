8dbb929d0fb40479415884842363fb2e83d92df2
Preksha <162373017+prekshasehgal@users.noreply.github.com>
2024-08-31 21:07:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+document.getElementById('darkmode-toggle').addEventListener('change', function() {
+    document.querySelector('.toggleBtn').style.backgroundColor = this.checked ? 'black' : 'white';
+});
+
+
+
+
+
+
+
+const words = ["Developer", "Designer", "Creator", "Innovator", "Yashik"];
+const dynamicElement = document.querySelector(".dynamic");
+let wordIndex = 0;
+
+dynamicElement.textContent ="Yashik"
+
+const longestWordLength = Math.max(...words.map(word => word.length));
+
+// Update the @keyframes rule
+const style = document.createElement('style');
+style.innerHTML = `
+  @keyframes dynamic {
+    0%, 100% {
+      width: 0;
+    }
+    50%, 80% {
+      width: ${longestWordLength}ch;
+    }
+  }
+`;
+document.head.appendChild(style);
+
+
+
+
+setInterval(() => {
+  dynamicElement.textContent = words[wordIndex];
+  wordIndex = (wordIndex + 1) % words.length;
+}, 3000);
